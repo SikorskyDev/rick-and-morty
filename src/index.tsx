1bdf2e4af04fb497dad1f98a453d6c41d553edf9
SIKORSKY YAROSLAV <mr.the.sikora@gmail.com>
2023-03-14 20:37:18 +0200

    registartion

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,18 @@ import App from "./pages/App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { Route, Routes } from "react-router-dom";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import CharacterDetails from "./pages/CharacterDetails";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <HashRouter>
-        <Provider store={store}>
-            <Routes>
+        <GoogleOAuthProvider clientId="286078022222-a6idklbcui1hbro51o3291fhem47n2k1.apps.googleusercontent.com">
+            <Provider store={store}>
+                <Routes>
                     <Route path="/" element={<App />} />
                     <Route path="/:id" element={<CharacterDetails />} />
                     <Route
@@ -23,7 +25,8 @@ root.render(
                             <div style={{ textAlign: "center" }}>Not Found</div>
                         }
                     />
-            </Routes>
-        </Provider>
+                </Routes>
+            </Provider>
+        </GoogleOAuthProvider>
     </HashRouter>
 );
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,6 +4,8 @@ import ImageByReact from "../components/ImageByReact/ImageByReact";
 import img1 from "../assets/img/rickAndMorty.png";
 import Search from "../components/Search/Search";
 import CharactersList from "../components/CharactersList/CharactersList";
+import { GoogleLogin } from "@react-oauth/google";
+import jwtDecode from "jwt-decode";
 
 const WrapperCSS = styled.div`
     min-height: 100vh;
@@ -19,11 +21,54 @@ const MainImageCSS = styled.div`
     max-height: 200px;
     margin-top: 86px;
     margin-bottom: 16px;
+    @media (max-width: 448px) {
+    margin-top: 50px;
+    }
+`;
+
+const AuthBarCSS = styled.div`
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`;
+const AuthBarTextCSS = styled.div`
+    justify-self: center;
+    align-self: center;
+`;
+
+const AuthBarBtnCSS = styled.div<any>`
+    display: ${(props) => props.value};
 `;
 
 function App() {
+    const [user, setUser] = React.useState<any>("");
+
     return (
         <WrapperCSS>
+            <AuthBarCSS>
+                <AuthBarTextCSS>
+                    {user !== "" ? `Hello ${user} !` : ""}
+                </AuthBarTextCSS>
+                <AuthBarBtnCSS value={user !== "" ? "none" : "block"}>
+                    <GoogleLogin
+                        onSuccess={(credentialResponse) => {
+                            console.log(credentialResponse);
+                            if (credentialResponse.credential) {
+                                let decode: any = jwtDecode(
+                                    credentialResponse.credential
+                                );
+                                setUser(decode.name);
+                            }
+                        }}
+                        onError={() => {
+                            console.log("Login Failed");
+                        }}
+                    />
+                </AuthBarBtnCSS>
+            </AuthBarCSS>
+
             <MainImageCSS>
                 <ImageByReact src={img1} />
             </MainImageCSS>
